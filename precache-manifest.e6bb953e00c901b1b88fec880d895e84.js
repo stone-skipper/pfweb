23b612438f0ca5149de1ad9703e77e1a4c93c067
stone-skipper <stone.skipper.ee@gmail.com>
2020-06-28 17:37:28 +0900

    Updates

---
+++ precache-manifest.e6bb953e00c901b1b88fec880d895e84.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d10c1992bc840a111c18a874ebdc9358",
+    "url": "/pfweb/index.html"
+  },
+  {
+    "revision": "4d71fee7d7fc54f53d9d",
+    "url": "/pfweb/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "5dc460dde9fe612e60ce",
+    "url": "/pfweb/static/js/2.838766c5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pfweb/static/js/2.838766c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d71fee7d7fc54f53d9d",
+    "url": "/pfweb/static/js/main.cf300f05.chunk.js"
+  },
+  {
+    "revision": "b2ccef5c68cdc5008c9f",
+    "url": "/pfweb/static/js/runtime-main.5064bdd9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/pfweb/static/media/logo.5d5d9eef.svg"
+  }
+]);
